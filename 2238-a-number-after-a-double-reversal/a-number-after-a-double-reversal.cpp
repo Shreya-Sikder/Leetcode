b31e3290d7cc12cbe3bc06f4a770c5afa5cdb613
Shreya Sikder <123325349+Shreya-Sikder@users.noreply.github.com>
2025-05-18 01:33:20 +0600

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.95%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        string s=to_string(num);
         
-             std::reverse(s.begin(), s.end());
-       if (num != 0 && num % 10 == 0) {
+        if (num != 0 && num % 10 == 0) {
             return false;
         }
         return true;
