974c0479a596dc213609d9053ca6ec019528ca98
Shreya Sikder <123325349+Shreya-Sikder@users.noreply.github.com>
2025-06-27 12:54:27 +0600

    Time: 26 ms (65.07%) | Memory: 16.3 MB (68.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int row, int column) {
-        // Mark current cell as visited
-        grid[row][column] = '0';
-
-        int rows = grid.size();
-        int cols = grid[0].size();
-
-        vector<int> dr = {1, -1, 0, 0};
-        vector<int> dc = {0, 0, 1, -1};
-
-        for (int i = 0; i < 4; i++) {
-            int r = row + dr[i];
-            int c = column + dc[i];
-            if (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '1') {
-                dfs(grid, r, c);
-            }
-        }
-    }
-
+   
+void dfs(int i,int j,vector<vector<char>>& grid){
+    if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]=='0')
+    return;
+    grid[i][j]='0';
+    dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+            dfs(i,j+1,grid);
+                dfs(i,j-1,grid);
+}
     int numIslands(vector<vector<char>>& grid) {
-        int row = grid.size();
-        if (row == 0) return 0;
-        int column = grid[0].size();//row te joto element totogulai column
-
-        int cnt = 0;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    cnt++;
-                }
-            }
+ int c=0;
+ for(int i=0;i<grid.size();i++){
+    for(int j=0;j<grid[0].size();j++){
+        if(grid[i][j]=='1'){
+            c++;
+            dfs(i,j,grid);
         }
-        return cnt;
     }
+ }
+
+return c;
+
+    }
+        
 };
